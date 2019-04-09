41e9b761e894f5ff6352c5c4d5dbb07cc7f8c6a2
chafla <namen937@gmail.com>
2019-04-09 17:34:28 -0400

    Pushing current progress
    I think the structure more or less makes sense, but
    there are some strange discrepancies in the file itself

---
+++ README.md
@@ -0,0 +1,6 @@
+# Midi Tests
+
+General tests in reading (and possibly) writing midis.
+
+This repo is really just for experimenting,
+and as a result will likely be all over the place.
--- main.c
+++ main.c
@@ -7,43 +7,118 @@ struct HeaderChunk {
     short division;
 };
 
+enum EventType{midi, meta, sysex};
+
 struct TrackEvent {
-    int td_to_next;  /// Timedelta offset before the next event
-    char status;
-    char channel_no;
-    int data_bytes;  /// Data of the message - one or two bytes
+    unsigned long td;  /// Timedelta offset before the current event
+    enum EventType event_type;
+    int status;
+    int data_bytes[2];  /// Data of the message - one or two bytes
 };
 
-struct TrackChunk {
-    int length;
-    struct TrackEvent *event;
+struct Track {
+    int id;
+    char *track_name;
+    struct TrackEvent **events;
 };
 
-struct TrackEvent *read_track_event(FILE *file) {
+// TODO will need to track meta events too
+
+/**
+ * Read in a track event and add it to its corresponding track
+ * @param file: file pointer to read from
+ * @param tracks: the track to read the track event from
+ */
+void read_track_events(FILE *file, struct Track *track) {
+
+    char vlv_timedelta[4];
+
+    char chunk_type[5];
+    int bytes_read = 0;
+    unsigned int data_length;
+    unsigned long timedelta;
+    unsigned int read_buf;
 
     struct TrackEvent *event = calloc(1, sizeof(struct TrackEvent));
-    char chunk_type[5];
 
     // parse header
     fread(chunk_type, sizeof(char), 4, file);
     chunk_type[4] = '\0';
+
     if (strcmp(chunk_type, "MTrk") != 0) {
         printf("Invalid chunk type, found %s\n", chunk_type);
-        return NULL;
+        return;
     }
 
-    // skip chunk type
-    fread(chunk_type, sizeof(char), 8, file);
+    // get the number of bytes contained by data segment
+    fread(&data_length, sizeof(int), 1, file);
+
+    // here's where the fun begins
+    // we need to run through data_length bytes and extract events
+
+    while (bytes_read < data_length) {
+
+        timedelta = 0;
+
+        // read in the timedelta between the past event and the current event
+        // it's formatted in a variable length, so the lower 7 bits are data.
+        // a 0 in the msb of a byte (like 01111111) means that the byte is the last in a variable-length number
+
+        do {
+
+            bytes_read += fread(&read_buf, sizeof(char), 1, file);
+
+            // thanks http://www.ccarh.org/courses/253/handout/vlv/
+
+            timedelta <<= (unsigned) 7;  // ignore the extra bit at the top of the value
+            timedelta |= (read_buf & (unsigned) 0x7F);
 
 
 
+            // Keep reading until we've read the 0 bit
+        } while (read_buf & (unsigned) 0x7);
+
+        event->td = timedelta;
+
+        // status byte
+
+        fread(&event->status, sizeof(int), 1, file);
+
+        // midi event
+        if (event->status <= 0xF0) {
+            // first byte is event specifier, second is
+            event->event_type = midi;
+            int event_code = event->status >> 8;
+            // these two events only have one data byte
+            if (event_code == 0xC || event_code == 0xD) {
+                fread(&event->data_bytes[1], sizeof(int), 1, file);
+            }
+            else {
+                fread(event->data_bytes, sizeof(int), 2, file);
+            }
+        }
+
+
+
+
+    }
+
+}
+
+/// Return a new track
+struct Track *create_track(int id) {
+    struct Track *new_track = calloc(1, sizeof(struct Track));
+    new_track->id = id;
+    new_track->track_name = NULL;
+    new_track->events = NULL;
+
+    return new_track;
 }
 
 /// read in a header chunk
 struct HeaderChunk *read_header_chunk(FILE *file) {
     // create a struct
     struct HeaderChunk *chunk = calloc(1, sizeof(struct HeaderChunk));
-    int data_len;
     char chunk_type[5];
     // skip over the chunk type
     fread(chunk_type, sizeof(char), 4, file);
@@ -53,7 +128,6 @@ struct HeaderChunk *read_header_chunk(FILE *file) {
         return NULL;
     }
     printf("Chunk type: %s\n", chunk_type);
-//    fseek(file, 4, SEEK_CUR);
 
     // it's just 6 bytes
 
@@ -62,6 +136,8 @@ struct HeaderChunk *read_header_chunk(FILE *file) {
 
     fread(&(chunk->n_tracks), 2, 1, file);
     fread(&(chunk->division), 2, 1, file);
+    // skip
+    fseek(file, sizeof(char) * 4, SEEK_CUR);
 
     return chunk;
 
@@ -74,24 +150,34 @@ struct HeaderChunk *read_header_chunk(FILE *file) {
 
 int main() {
 
-    FILE *midi = fopen("002 - Top Menu Theme.mid", "rb");
-    struct TrackEvent *events;
+    FILE *midi = fopen("PMD-Explorers of Sky MIDI RIP/002 - Top Menu Theme.mid", "rb");
+    struct Track **tracks;
     if (!midi) {
         perror("Error when loading file");
         return EXIT_FAILURE;
     }
     struct HeaderChunk *header;
 
+    // create header chunk
     header = read_header_chunk(midi);
-    events = calloc(sizeof(struct TrackEvent), header->n_tracks);
+
+    // create all the tracks that we will be using
+    tracks = calloc(sizeof(size_t), header->n_tracks);
+
     for (int i = 0; i < header->n_tracks; i++) {
 
-        events[i] =
+        tracks[i] = create_track(i);
+        read_track_events(midi, tracks[i]);
+
+        // now, we'll go through the rest of the midi file.
+        // the structure is organized such that there are track chunks for every logical track in the file
+        // (think instruments).
 
     }
 
+
+
     printf("n_tracks: %d, division: %d\n", header->n_tracks, header->division);
 
-    printf("Hello, World!\n");
     return 0;
 }
